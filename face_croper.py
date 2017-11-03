3d4a65ccd60761cc68b4bba2f5e5afa8f016b8d3
Paweł Rakosza <229793@student.pwr.edu.pl>
2017-11-03 12:35:21 +0100

    Better quality

--- face_croper.py
+++ face_croper.py
@@ -1,4 +1,4 @@
-#Pierwszy argument ścieżka na folder z obrazami 
+#Pierwszy argument ścieżka na folder z obrazami
 import sys
 import os
 import dlib
@@ -10,14 +10,14 @@ try:
     os.mkdir(os.path.join(sys.argv[1],r'faces'))
 except FileExistsError:
     print("Folder exists")
-
+quality=[int(cv2.IMWRITE_JPEG_QUALITY), 100]
 for f in glob.glob(os.path.join(sys.argv[1], "*.jpg")):
     print("Processing file: {}".format(f))
-    img = io.imread(f)
+    img = cv2.imread(f)
     dets = detector(img, 1)
     for i, d in enumerate(dets):
         crop_img = img[d.top():d.bottom(), d.left():d.right()]
-        cv2.imwrite(os.path.join(sys.argv[1],(r'faces'+r'\face'+str(i)+os.path.basename(f))), crop_img)
+        cv2.imwrite(os.path.join(sys.argv[1],(r'faces'+r'\face'+str(i)+os.path.basename(f))), crop_img,quality)
 
 
 
