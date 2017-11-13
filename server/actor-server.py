5c2554ac6250adf7c6f6ffcfee144bdcd9077555
Unknown <229761@student.pwr.edu.pl>
2017-11-13 12:18:30 +0100

    Test small change
    Needed to test desktop client

--- server/actor-server.py
+++ server/actor-server.py
@@ -1,33 +1,34 @@
 from flask import Flask, Response, jsonify, request
 from actorinfo import ActorsInfoPicker, Actor, MovieCredit
 from werkzeug import secure_filename
+# from werkzeug.utils import secure_filename
 
 import json
-
 
 app = Flask(__name__)
 
 
 @app.route('/actors/image', methods=['POST'])
 def getActors():
-	img = request.files['image'];
+    img = request.files['image']
 
-	img.save(secure_filename(img.filename))
-	# 		TODO
-	# Use image to run AI
-	# find actor's name for specific id
-	# create JSON with array of id, name, position
-	#
+    img.save(secure_filename(img.filename))
+    # 		TODO
+    # Use image to run AI
+    # find actor's name for specific id
+    # create JSON with array of id, name, position
+    #
 
-	# test connection
-	dicted_actors = dict(status = 'ok')	
-	data = json.dumps(dicted_actors)
-	
-	return Response(data, mimetype="application/json")
+    # test connection
+    # dicted_actors = dict(status='ok')
+    dicted_actors = []
+    dicted_actors.append(dict(name='Alan Rickman', imdb='nm0000614'))
+    data = json.dumps(dicted_actors)
+
+    return Response(data, mimetype="application/json")
 
 
-
-#JSON with details contains 
+# JSON with details contains
 # 	array of Actor:
 #		name: string
 #		biography: string
@@ -44,42 +45,40 @@ def getActors():
 #	
 @app.route('/actordetails/<actor_id>', methods=['GET'])
 def getDetails(actor_id):
-	picker = ActorsInfoPicker()
-	actors = []
-	
-	actors.append(picker.download_by_imdb_id(actor_id))
+    picker = ActorsInfoPicker()
+    actors = []
 
-	# Preparing JSON with actor details to return
-	dicted_actors = []
-	for actor in actors:
-		dicted_credits = []
-		for movie_credit in actor.movie_credits:
-			
-			dicted_credits.append(dict(
-					title = movie_credit.title,
-					genres = movie_credit.genres,
-					vote_average = movie_credit.vote_average,
-					poster = movie_credit.poster
-				))
+    #       TODO
+    # find actor's tmdb id for given imdb id
+    actors.append(picker.download_actor_info('4566', 'nm0000614'))
 
-		temp = dict(
-				name = actor.name,
-				biography = actor.biography,
-				birthday = actor.birthday,
-				deathday = actor.deathday,
-				gender = actor.gender,
-				imdb_profile = actor.imdb_profile,
-				images = actor.images,
-				movie_credits = dicted_credits
-			)
-		dicted_actors.append(temp)
-	data = json.dumps(dicted_actors)
+    # Preparing JSON with actor details to return
+    dicted_actors = []
+    for actor in actors:
+        dicted_credits = []
+        for movie_credit in actor.movie_credits:
+            dicted_credits.append(dict(
+                title=movie_credit.title,
+                genres=movie_credit.genres,
+                vote_average=movie_credit.vote_average,
+                poster=movie_credit.poster
+            ))
 
-	return Response(data, mimetype="application/json")
+        temp = dict(
+            name=actor.name,
+            biography=actor.biography,
+            birthday=actor.birthday,
+            deathday=actor.deathday,
+            gender=actor.gender,
+            imdb_profile=actor.imdb_profile,
+            images=actor.images,
+            movie_credits=dicted_credits
+        )
+        dicted_actors.append(temp)
+    data = json.dumps(dicted_actors)
 
+    return Response(data, mimetype="application/json")
 
 
 if __name__ == '__main__':
     app.run()
-
-
