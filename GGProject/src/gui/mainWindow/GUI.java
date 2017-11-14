590a5f862c58abb754803344e27f9e6000aacb41
BlackEtude <229726@student.pwr.edu.pl>
2017-11-14 13:44:22 +0100

    App class as main class (logic run from the inside) and some gui fixes

--- GGProject/src/gui/mainWindow/GUI.java
+++ GGProject/src/gui/mainWindow/GUI.java
@@ -1,6 +1,7 @@
 package gui.mainWindow;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -11,9 +12,10 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import main.Main;
+import models.Actor;
 
 /**
  * open in browser (for imdb "More info")
@@ -26,38 +28,52 @@ import javafx.stage.Stage;
 //		e.printStackTrace();
 //	}
 
+/**
+ * Main GUI class with JavaFX components.
+  */
 public class GUI extends Application {
 	private Stage stage;
 	private Scene scene;
-	private BorderPane mainPane;
-	private ScrollPane rightPane;
-	private GridPane actorPane;
-	private Pane imagePane;
-	private MenuBar menuBar;
+	private static BorderPane mainPane;
+	private static ScrollPane rightPane;
+	private static GridPane actorPane;
+	private static GridPane imagePane;
+	private static MenuBar menuBar;
+
+	//Actor fields
+	private static Text nameField, birthdateField, biographyField, diedField, genderField;
 
 	public void start(Stage primaryStage) {
+		Main mainThread = new Main();
+		mainThread.start();
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 		stage = primaryStage;
 		primaryStage.setTitle("JavaFX GG");
 
 		mainPane = new BorderPane();
 		mainPane.setPrefSize(1000, 470);
 		rightPane = new ScrollPane();
-		imagePane = new Pane();
+		imagePane = new GridPane();
 		menuBar = new MenuBar();
 
-		setMenuBar();
-		setImagePane();
 		setActorPane();
-		setRightPane();
-
+		setMenuBar();
+		rightPane.setContent(actorPane);
 		mainPane.setTop(menuBar);
 		mainPane.setCenter(imagePane);
 		mainPane.setRight(rightPane);
 		BorderPane.setAlignment(menuBar, Pos.TOP_CENTER);
 		BorderPane.setAlignment(imagePane, Pos.CENTER_LEFT);
 		BorderPane.setAlignment(rightPane, Pos.CENTER_RIGHT);
-
 		setUserAgentStylesheet(STYLESHEET_MODENA);
+
+		System.out.println("Created");
 
 		scene = new Scene(mainPane);
 		primaryStage.setScene(scene);
@@ -65,37 +81,15 @@ public class GUI extends Application {
 		primaryStage.show();
 	}
 
-	private void setRightPane() {
-//		rightPane.setFitToWidth(true);
-		rightPane.setMaxWidth(350);
-		ImageView photo = new ImageView("File:images/photo.jpg");
-		photo.setFitHeight(100);
-		photo.setFitWidth(100);
-		//leave it commented, oth - image proportions will be respected
-		photo.setPreserveRatio(true);
-//		photo.fitWidthProperty().bind(imagePane.widthProperty());
-//		photo.fitHeightProperty().bind(imagePane.heightProperty());
-//		rightPane.setContent(photo);
-		rightPane.setContent(actorPane);
-	}
 
-	private void setMenuBar() {
+	private static void setMenuBar() {
 		Menu menuFile = new Menu("File");
 		Menu menuEdit = new Menu("Edit");
 		Menu menuView = new Menu("View");
 		menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
 	}
 
-	private void setImagePane() {
-		ImageView image = new ImageView("File:screens/20171106_02_11_11_PM.png");
-		//leave it commented, oth - image proportions will be respected
-//		image.setPreserveRatio(true);
-		image.fitWidthProperty().bind(imagePane.widthProperty());
-		image.fitHeightProperty().bind(imagePane.heightProperty());
-		imagePane.getChildren().addAll(image);
-	}
-
-	private void setActorPane() {
+	private static void setActorPane() {
 		actorPane = new GridPane();
 //		actorPane.setAlignment(Pos.CENTER);
 		actorPane.setHgap(10);
@@ -109,27 +103,15 @@ public class GUI extends Application {
 		Label name = new Label("Name: ");
 		Label birthdate = new Label("Birthdate: ");
 		Label died = new Label("Died: ");
+		Label gender = new Label("Gender");
 		Label biography = new Label("Biography: ");
-		Label knownFrom = new Label("Known from: ");
 
-		Text nameField = new Text("Alan Sidney Patrick Rickman bla bla bla");
-		Text birthdateField = new Text("21 February 1946");
-		Text diedField = new Text("14 January 2016 (aged 69)");
-		Text biographyField = new Text("Alan Rickman was born on a council " +
-				"estate in Acton, West London, to Margaret Doreen Rose (Bartlett) " +
-				"and Bernard Rickman, who worked at a factory. He had English, Irish, " +
-				"and Welsh ancestry. Alan had an older brother David, a younger brother Michael " +
-				"and a younger sister Sheila. When Alan was 8 years old, his father died. He attended " +
-				"Latymer Upper School on a scholarship. He studied Graphic Design at Chelsea College of Art " +
-				"and Design, where he met Rima Horton, who would later become his life partner. After three " +
-				"years at Chelsea College, Rickman did graduate studies at the Royal College of Art. He " +
-				"opened a successful graphics design business, Graphiti, with friends and ran it for several " +
-				"years before his love of theatre led him to seek an audition with the Royal Academy of Dramatic " +
-				"Art (RADA). At the relatively late age of 26, Rickman received a scholarship to RADA, which" +
-				" started a professional acting career that has lasted nearly 40 years, a career which has spanned " +
-				"stage, screen and television and lapped over into directing, as well.");
-//		biographyField.maxWidth(100);
-		nameField.autosize();
+		nameField = new Text();
+		birthdateField = new Text();
+		diedField = new Text();
+		biographyField = new Text();
+		genderField = new Text();
+
 		actorPane.add(name, 0, 3);
 		actorPane.add(nameField, 1,3);
 		actorPane.add(birthdate, 0,4);
@@ -137,8 +119,33 @@ public class GUI extends Application {
 		actorPane.add(died, 0,5);
 		actorPane.add(diedField, 1,5);
 		actorPane.add(biography, 0, 6);
-//		actorPane.add(biographyField, 0, 7);
-		actorPane.add(knownFrom, 0, 8);
+		actorPane.add(biographyField, 0, 7);
+
+		actorPane.add(gender, 0, 8);
+		actorPane.add(genderField, 1, 8);
+	}
+
+	public static void setScreenshot(String screenPath) {
+		ImageView image = new ImageView(screenPath);
+//		image.setPreserveRatio(true);
+		image.fitWidthProperty().bind(imagePane.widthProperty());
+		image.fitHeightProperty().bind(imagePane.heightProperty());
+		Platform.runLater(() -> imagePane.add(image, 0, 0));
+	}
+
+	public static void loadActor(Actor actorData) {
+		nameField.setText(actorData.getName());
+		birthdateField.setText(actorData.getBirthday());
+		diedField.setText(actorData.getDeathday());
+		biographyField.setText("Problem with text wrapping. Fix soon");
+		genderField.setText(actorData.getGender());
+		System.out.println(actorData.getImdb_profile());
+
+		ImageView photo = new ImageView(actorData.getImages().get(0));
+//		photo.setFitHeight(100);
+		photo.setFitWidth(250);
+		photo.setPreserveRatio(true);
+		Platform.runLater(() -> actorPane.add(photo, 0, 0));
 	}
 
 	public static void main(String[] args) {
--- GGProject/src/main/Main.java
+++ GGProject/src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import Connection.NewClient;
 import Screens.GlobalKeyListener;
 import gui.TrayGUI;
+import gui.mainWindow.GUI;
 import json.JSONModelParser;
 import models.Actor;
 import models.SimpleActor;
@@ -11,24 +12,28 @@ import org.json.JSONArray;
 import javax.swing.*;
 import java.util.ArrayList;
 
-public class Main {
-	public static NewClient client;
-	public static void main(String args[]) {
-//		Client client = new Client();
-//		client.startConnection("localhost", 5000);
+public class Main extends Thread{
+	private static NewClient client;
+
+	public void run () {
+		SwingUtilities.invokeLater(TrayGUI::new);
+		GlobalKeyListener.startListener();
 
         /*testing new client*/
         NewClient client = new NewClient();
 		Main.client = client;
         JSONModelParser parser= new JSONModelParser();
-		JSONArray actor = client.postRequest("http://127.0.0.1:5000/actors/image", "images/photo.jpg");
+		JSONArray actor = client.postRequest("http://127.0.0.1:5000/actors/image", "screens/20171114_11_34_25_AM.png");
 		ArrayList<SimpleActor> simpleActors = parser.parseToSimpleActor(actor);
 		System.out.println(simpleActors.get(0).getName());
+
         actor = client.getRequest("http://127.0.0.1:5000/actordetails/nm0000614");
         ArrayList<Actor> actors =  parser.parseToActor(actor);
         System.out.println(actors.get(0).getBiography());
 
-		SwingUtilities.invokeLater(TrayGUI::new);
-		GlobalKeyListener.startListener();
+        //run gui updater after json was received
+		GUI.setScreenshot("File:screens/20171114_11_34_25_AM.png");
+		GUI.loadActor(actors.get(0));
+
 	}
 }
