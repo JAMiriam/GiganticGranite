77f176a1373b1bec2e609d354842114a7aac6b6c
BlackEtude <229726@student.pwr.edu.pl>
2017-12-03 16:39:21 +0100

    details pane update, working program

---
+++ GGProject/src/main/java/gui/detailsWindow/DetailsPane.java
@@ -0,0 +1,5 @@
+package gui.detailsWindow;
+
+public class DetailsPane {
+
+}
--- GGProject/src/main/java/transmission/Client.java
+++ GGProject/src/main/java/transmission/Client.java
@@ -2,15 +2,20 @@ package transmission;
 
 import gui.WindowManager;
 import json.JSONModelParser;
+import models.Actor;
 import models.SimpleActor;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.routing.HttpRoutePlanner;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 
@@ -20,24 +25,25 @@ import java.util.ArrayList;
 
 
 public class Client {
-    //	private static final String SERVER_IP = "25.29.196.52";
+//    	private static final String SERVER_IP = "25.29.196.52";
     private static final String SERVER_IP = "192.168.0.101";
     private static final int SERVER_HOST = 5000;
     private static CloseableHttpClient httpClient;
 
-    public Client(String serverIp, int serverHost) {
+    public Client() {
         httpClient = HttpClientBuilder.create().build();
 
         //Throughout proxy
-//        HttpHost proxy = new HttpHost(serverIp, serverHost);
-//        HttpRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxy);
-//        httpClient = HttpClients.custom().setRoutePlanner(routePlanner).build();
+        HttpHost proxy = new HttpHost(SERVER_IP, SERVER_HOST);
+        HttpRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxy);
+        httpClient = HttpClients.custom().setRoutePlanner(routePlanner).build();
     }
 
     public static void sendImageToServer(String path) {
         JSONArray postResponse = postRequest("http://" + SERVER_IP + ":" + SERVER_HOST + "/actors/image", path);
         ArrayList<SimpleActor> simpleActorsList = JSONModelParser.parseToSimpleActor(postResponse);
         WindowManager.createSimpleWindow(simpleActorsList);
+        SessionData.getActorsData(simpleActorsList);
     }
 
     private static JSONArray postRequest(String url, String path) {
@@ -63,7 +69,7 @@ public class Client {
         return actor;
     }
 
-    public static JSONArray getRequest(String url) {
+    private static JSONArray getRequest(String url) {
         JSONArray actor = new JSONArray();
         HttpGet get = new HttpGet(url);
 
@@ -76,6 +82,12 @@ public class Client {
             e.printStackTrace();
         }
         return actor;
+    }
+
+    static ArrayList<Actor> getDetails(String imdb_id) {
+        JSONArray array = getRequest("http://" + SERVER_IP + ":" + SERVER_HOST + "/actordetails/" + imdb_id);
+        ArrayList<Actor> actors =  JSONModelParser.parseToActor(array);
+        return actors;
     }
 
     public static void closeConnection() {
