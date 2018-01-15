26480de33676412138d25270a82aa27b0a774090
BlackEtude <229726@student.pwr.edu.pl>
2018-01-15 14:27:28 +0100

    Https connection with server

--- GGProject/src/main/java/transmission/Client.java
+++ GGProject/src/main/java/transmission/Client.java
@@ -5,21 +5,26 @@ import json.JSONModelParser;
 import models.Actor;
 import models.Complaint;
 import models.SimpleActor;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.*;
+import org.apache.http.client.methods.*;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
+import org.apache.http.impl.client.*;
+import org.apache.http.ssl.*;
+import org.apache.http.util.*;
 import org.json.JSONArray;
 
+import javax.net.ssl.SSLContext;
 import java.io.File;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Vector;
 
 
 public class Client {
@@ -27,21 +32,40 @@ public class Client {
 //    static final String SERVER_IP = "192.168.0.101";
     static final int SERVER_HOST = 5000;
     private static CloseableHttpClient httpClient;
+    public static Vector<String> screenshotPaths;
 
     public Client() {
-        httpClient = HttpClientBuilder.create().build();
+    	httpClient = HttpClients.createDefault();
+
+		try {
+		SSLContext sslContext = new SSLContextBuilder()
+				.loadTrustMaterial(null, (certificate, authType) -> true).build();
+
+		httpClient = HttpClients.custom()
+				.setSSLContext(sslContext)
+				.setSSLHostnameVerifier(new NoopHostnameVerifier())
+				.build();
+
+		} catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
+			e.printStackTrace();
+			//TODO no connection - countdown for retry
+		}
+
+        screenshotPaths = new Vector<>();
     }
+
 
     public static void sendImageToServer(String path) {
         SessionData.setScreenshotPath(path);
-        JSONArray postResponse = postRequest("http://" + SERVER_IP + ":" + SERVER_HOST + "/actors/image", path);
+        screenshotPaths.add(path);
+        JSONArray postResponse = postRequest("https://" + SERVER_IP + ":" + SERVER_HOST + "/actors/image", path);
         ArrayList<SimpleActor> simpleActorsList = JSONModelParser.parseToSimpleActor(postResponse);
         GUIManager.createSimpleWindow(simpleActorsList);
         SessionData.getActorsData(simpleActorsList);
     }
         
     public static void sendSuggestionToServer(Complaint complaint) {
-        String ret = postComplaint("http://" + SERVER_IP + ":" + SERVER_HOST + "/actors/suggestion", complaint);
+        String ret = postComplaint("https://" + SERVER_IP + ":" + SERVER_HOST + "/actors/suggestion", complaint);
         System.out.println(ret);
     }
 
@@ -58,7 +82,7 @@ public class Client {
         post.setEntity(entity);
 
         System.out.println("Executing request: \"" + post.getRequestLine() + "\"");
-        String resp = ":(";
+        String resp = "";
         try {
             resp = EntityUtils.toString(httpClient.execute(post).getEntity(), "UTF-8");
         } catch (Exception e) {
@@ -107,12 +131,10 @@ public class Client {
     }
 
     static ArrayList<Actor> getDetails(String imdb_id) {
-        JSONArray array = getRequest("http://" + SERVER_IP + ":" + SERVER_HOST + "/actordetails/" + imdb_id);
+        JSONArray array = getRequest("https://" + SERVER_IP + ":" + SERVER_HOST + "/actordetails/" + imdb_id);
         ArrayList<Actor> actors =  JSONModelParser.parseToActor(array);
         return actors;
     }
-
-
 
     public static void closeConnection() {
         try {
