8d968063372637921ee01e5c27ed2e700db43b01
Michał Krasoń <kramichal96@gmail.com>
2018-01-15 20:27:41 +0100

    removed remember me checkbox

--- website/giganticgranite.com/public_html/index.php
+++ website/giganticgranite.com/public_html/index.php
@@ -88,11 +88,6 @@
                                 <?php
                             } ?>
                         </div>
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" value="remember-me"> Remember me
-                            </label>
-                        </div>
                         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
                     </form>
                 </div>
